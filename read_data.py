68d6eb67cc9785b6498d8e1bcf1dec2746d1a0fc
u0940253 <u0940253@utah.edu>
2020-11-03 13:19:21 -0700

    update best context function

--- read_data.py
+++ read_data.py
@@ -172,21 +172,48 @@ def get_best_context_w_pos(story, story_pos, question, question_pos,k):
     return best_context
 
 
-def get_best_context_w_weight(story, questions, k):
-    best_context_matches = 0
+#input: story & q are spacy objs. q_type is the question type, weight dict defines how to distubute weights among attributes
+def get_best_context_w_weight(story, question, attribute_dict, k, q_type, weight_dict):
+    best_context_weight = 0
     for t_idx in range(len(story)):
         context_words = get_context_words_span(story, k, t_idx)
-        curr_context_matches = 0
         # for q_word in question.split():
-        for q_word in questions:
+        curr_context_weight=0
+
+        #word level comparisons
+        for q_word in question:
             # TODO: if duplicates exist, this still only counts 1
             # Could add a custom scoring function here
             for s_word in context_words:
-                curr_context_matches+= q_word.similarity(s_word)
-        if curr_context_matches > best_context_matches:
-            best_context_matches = curr_context_matches
+                for w_type in weight_dict: 
+                    if(w_type== 'TEXT'):
+                        curr_context_weight+= q_word.similarity(s_word)*weight_dict[w_type]
+                    elif(w_type=='POS'):
+                        curr_attr=attribute_dict[q_type][w_type]
+                        if(s_word in curr_attr):
+                            curr_context_weight+=curr_attr[s_word.pos_]*weight_dict[w_type]
+                    elif(w_type=='ENT'):
+                        continue
+                        # entities= [ent.label_ for ent in context_doc.ents]
+                        # for ent in entities:
+                        #     if ent in curr_attr:
+                        #         curr_context_weight+=curr_attr[ent]*weight_dict[w_type]
+                    else:
+                        print("Attribute Type currently not supported")
+        #context level comparisons
+        if('ENT' in weight_dict):
+            # context_doc=vectorize_list(context_words)
+            entities= [ent.label_ for ent in context_words.ents]
+            curr_attr=attribute_dict[q_type]["ENT"]
+            for ent in entities:
+                if ent in curr_attr:
+                   curr_context_weight+=curr_attr[ent]*weight_dict[w_type]
+
+        # print(curr_context_weight)
+        if curr_context_weight > best_context_weight:
+            best_context_weight = curr_context_weight
             best_context = context_words
-    print(best_context_matches)
+    print(best_context_weight)
     return best_context
 
 
@@ -207,14 +234,14 @@ def filter_by_stopwords(text, stopwords):
     return filtered_text
 
 
-def vectorize_words(text):
+def vectorize_list(text):
     str1 = ""  
     
     # traverse in the string   
     for ele in text:  
         str1 += ele+" "   
     # print(str1)
-    vectorized=vec_model(str1)
+    vectorized=nlp(str1)
     # print(vectorized)
     return vectorized
 
@@ -278,7 +305,7 @@ for story_id in list(questions.keys()):
 
 # q_2word_counts = {k: v for k, v in sorted(q_2word_counts.items(), key=lambda item: item[1], reverse=True)}
 print(q_2word_counts)
-print(id_to_type)
+# print(id_to_type)
 # Find the best context- section of story words with most overlap of questions words
 k = 5
 for story_id in list(questions.keys()):
@@ -291,13 +318,13 @@ for story_id in list(questions.keys()):
         tokenized_q = nltk.word_tokenize(question)
         q_type=id_to_type[question_id]
         for a in answer:
-            print("Question: ", question)
-            print("Answer: ", a)
+            # print("Question: ", question)
+            # print("Answer: ", a)
             doc = nlp(a)
-            print("Named Entities: ", [[ent.text, ent.label_] for ent in doc.ents])
-            print("Noun phrases: ", [nc.text for nc in doc.noun_chunks]) #todo not sure how to implement in best context so left out of dict
-            print("Text as POS tags: ", [token.pos_ for token in doc])
-            print('\n')
+            # print("Named Entities: ", [[ent.text, ent.label_] for ent in doc.ents])
+            # print("Noun phrases: ", [nc.text for nc in doc.noun_chunks]) #todo not sure how to implement in best context so left out of dict
+            # print("Text as POS tags: ", [token.pos_ for token in doc])
+            # print('\n')
 
             for ent in doc.ents:
                 if ent.label_ not in q_2word_counts[q_type]['ENT']:
@@ -317,36 +344,46 @@ print(q_2word_counts)
 
 print('here')
 
+weights={"TEXT":1, "POS":.1, "ENT":1}
+for story_id in list(questions.keys()):
+    story_qa = questions[story_id]
+    story = stories[story_id]['TEXT']
+    # print(story)
+    tokenized_s = nltk.word_tokenize(story)
+    for question_id in list(story_qa.keys()):
+        question = story_qa[question_id]['Question']
+        answer = story_qa[question_id]['Answer'] #this is a list
+        tokenized_q = nltk.word_tokenize(question)
+        q_type=id_to_type[question_id]
 
-        # tokenized_q = nltk.word_tokenize(question)
-        # tokenized_s = nltk.word_tokenize(story)
-
-        # filtered_q, filtered_q_pos = filter_by_POS(tokenized_q, ['DT', '.', ','])
-        # # filtered_s_text, filtered_s_pos = filter_by_POS(tokenized_s, ['DT', '.', ','])
-        #
-        # filtered_q = filter_by_stopwords(filtered_q, stop_words)
-        # filtered_s = filter_by_stopwords(story, stop_words)
-        #
-        # vectorized_q = vectorize_words(filtered_q)
-        # vectorized_s = vectorize_words(filtered_s)
-        # # for token in vectorized_q:
-        # #     if not token.has_vector:
-        # #         print('AAAHHH',token.text, token.has_vector, token.vector_norm, token.is_oov)
-        # best_context = get_best_context_w_weight(vectorized_s,vectorized_q,k)
-        # # # Build synonym list for words in question
-        # # # TODO: clean this list
-        # # synonyms = []
-        # # for word in filtered_q:
-        # #     for synset in wordnet.synsets(word):
-        # #         for lemma in synset.lemmas():
-        # #             synonyms.append(lemma.name())
-        # # filtered_q.extend(synonyms)
-        #
-        # # best_context = get_best_context(filtered_s, filtered_q, k)
-        # print(question)
-        # print(story_qa[question_id]['Answer'])
-        # print(best_context)
-        # # best_context_w_pos=get_best_context_w_pos(filtered_s_text, filtered_s_pos,filtered_q_text,filtered_q_pos,k)
-        # print('here')
+        filtered_q, filtered_q_pos = filter_by_POS(tokenized_q, ['DT', '.', ','])
+        filtered_s_text, filtered_s_pos = filter_by_POS(tokenized_s, ['DT', '.', ','])
+        
+        filtered_q = filter_by_stopwords(filtered_q, stop_words)
+        filtered_s = filter_by_stopwords(filtered_s_text, stop_words)
+        # print(filtered_s)
+        vectorized_q = vectorize_list(filtered_q)
+        vectorized_s = vectorize_list(filtered_s)
+        # for token in vectorized_q:
+        #     if not token.has_vector:
+        #         print('AAAHHH',token.text, token.has_vector, token.vector_norm, token.is_oov)
+        # print(vectorized_q, vectorized_s)
+        best_context = get_best_context_w_weight(vectorized_s,vectorized_q,q_2word_counts,k,q_type,weights)
+        # # Build synonym list for words in question
+        # # TODO: clean this list
+        # synonyms = []
+        # for word in filtered_q:
+        #     for synset in wordnet.synsets(word):
+        #         for lemma in synset.lemmas():
+        #             synonyms.append(lemma.name())
+        # filtered_q.extend(synonyms)
+        
+        # best_context = get_best_context(filtered_s, filtered_q, k)
+        print(question)
+        print(story_qa[question_id]['Answer'])
+        print(best_context)
+        # best_context_w_pos=get_best_context_w_pos(filtered_s_text, filtered_s_pos,filtered_q_text,filtered_q_pos,k)
+        print('here')
+    sadf
 #==========
 
