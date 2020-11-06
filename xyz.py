b7b6d63d3b0ef76c939ba80fc0319d4acc634486
u0940253 <u0940253@utah.edu>
2020-11-06 13:38:23 -0700

    fix qa errors

--- xyz.py
+++ xyz.py
@@ -23,6 +23,8 @@ print("l")
 for typ in my_dictl:
     if my_dictl[typ]["text_weight"]!=0:
         if typ not in added:
+            my_final_dict[typ]={}
+
             print('for type ', typ)
             print('text_weight', my_dictl[typ]["text_weight"])
             print('pos_weight', my_dictl[typ]["pos_weight"])
@@ -30,11 +32,17 @@ for typ in my_dictl:
             print('k', my_dictl[typ]["k"])
             print('bump_weight', my_dictl[typ]["bump_weight"])
             added.append(typ)
-            my_final_dict[typ]=my_dictl[typ]
+            my_final_dict[typ]['POS']=my_dictl[typ]["pos_weight"]
+            my_final_dict[typ]['ENT']=my_dictl[typ]["ent_weight"]
+            my_final_dict[typ]['BUMP']=my_dictl[typ]["bump_weight"]
+            my_final_dict[typ]['K']=my_dictl[typ]["k"]
+            my_final_dict[typ]['TEXT']=my_dictl[typ]["text_weight"]
 print('s')
 for typ in my_dict:
     if my_dict[typ]["text_weight"]!=0:
         if typ not in added:
+            my_final_dict[typ]={}
+
             print('for type ', typ)
             print('text_weight', my_dict[typ]["text_weight"])
             print('pos_weight', my_dict[typ]["pos_weight"])
@@ -42,12 +50,18 @@ for typ in my_dict:
             print('k', my_dict[typ]["k"])
             print('bump_weight', my_dict[typ]["bump_weight"])
             added.append(typ)
-            my_final_dict[typ]=my_dict[typ]
+            my_final_dict[typ]['POS']=my_dict[typ]["pos_weight"]
+            my_final_dict[typ]['ENT']=my_dict[typ]["ent_weight"]
+            my_final_dict[typ]['BUMP']=my_dict[typ]["bump_weight"]
+            my_final_dict[typ]['K']=my_dict[typ]["k"]
+            my_final_dict[typ]['TEXT']=my_dict[typ]["text_weight"]
 
 print('a')
 for typ in my_dicta:
     if my_dicta[typ]["text_weight"]!=0:
         if typ not in added:
+            my_final_dict[typ]={}
+
             print('for type ', typ)
             print('text_weight', my_dicta[typ]["text_weight"])
             print('pos_weight', my_dicta[typ]["pos_weight"])
@@ -55,12 +69,18 @@ for typ in my_dicta:
             print('k', my_dicta[typ]["k"])
             print('bump_weight', my_dicta[typ]["bump_weight"])
             added.append(typ)
-            my_final_dict[typ]=my_dicta[typ]
+            my_final_dict[typ]['POS']=my_dicta[typ]["pos_weight"]
+            my_final_dict[typ]['ENT']=my_dicta[typ]["ent_weight"]
+            my_final_dict[typ]['BUMP']=my_dicta[typ]["bump_weight"]
+            my_final_dict[typ]['K']=my_dicta[typ]["k"]
+            my_final_dict[typ]['TEXT']=my_dicta[typ]["text_weight"]
 
 print('b')
 for typ in my_dictb:
     if my_dictb[typ]["text_weight"]!=0:
         if typ not in added:
+            my_final_dict[typ]={}
+
             print('for type ', typ)
             print('text_weight', my_dictb[typ]["text_weight"])
             print('pos_weight', my_dictb[typ]["pos_weight"])
@@ -68,12 +88,18 @@ for typ in my_dictb:
             print('k', my_dictb[typ]["k"])
             print('bump_weight', my_dictb[typ]["bump_weight"])
             added.append(typ)
-            my_final_dict[typ]=my_dictb[typ]
+            my_final_dict[typ]['POS']=my_dictb[typ]["pos_weight"]
+            my_final_dict[typ]['ENT']=my_dictb[typ]["ent_weight"]
+            my_final_dict[typ]['BUMP']=my_dictb[typ]["bump_weight"]
+            my_final_dict[typ]['K']=my_dictb[typ]["k"]
+            my_final_dict[typ]['TEXT']=my_dictb[typ]["text_weight"]
 
 print('c')
 for typ in my_dictc:
     if my_dictc[typ]["text_weight"]!=0:
         if typ not in added:
+            my_final_dict[typ]={}
+
             print('for type ', typ)
             print('text_weight', my_dictc[typ]["text_weight"])
             print('pos_weight', my_dictc[typ]["pos_weight"])
@@ -81,11 +107,17 @@ for typ in my_dictc:
             print('k', my_dictc[typ]["k"])
             print('bump_weight', my_dictc[typ]["bump_weight"])
             added.append(typ)
-            my_final_dict[typ]=my_dictc[typ]
+            my_final_dict[typ]['POS']=my_dictc[typ]["pos_weight"]
+            my_final_dict[typ]['ENT']=my_dictc[typ]["ent_weight"]
+            my_final_dict[typ]['BUMP']=my_dictc[typ]["bump_weight"]
+            my_final_dict[typ]['K']=my_dictc[typ]["k"]
+            my_final_dict[typ]['TEXT']=my_dictc[typ]["text_weight"]
 print('wa')
 for typ in my_dictwa:
     if my_dictwa[typ]["text_weight"]!=0:
         if typ not in added:
+            my_final_dict[typ]={}
+
             print('for type ', typ)
             print('text_weight', my_dictwa[typ]["text_weight"])
             print('pos_weight', my_dictwa[typ]["pos_weight"])
@@ -93,12 +125,18 @@ for typ in my_dictwa:
             print('k', my_dictwa[typ]["k"])
             print('bump_weight', my_dictwa[typ]["bump_weight"])
             added.append(typ)
-            my_final_dict[typ]=my_dictwa[typ]
+            my_final_dict[typ]['POS']=my_dictwa[typ]["pos_weight"]
+            my_final_dict[typ]['ENT']=my_dictwa[typ]["ent_weight"]
+            my_final_dict[typ]['BUMP']=my_dictwa[typ]["bump_weight"]
+            my_final_dict[typ]['K']=my_dictwa[typ]["k"]
+            my_final_dict[typ]['TEXT']=my_dictwa[typ]["text_weight"]
 
 print('r2')
 for typ in my_dictr2:
     if my_dictr2[typ]["text_weight"]!=0:
         if typ not in added:
+            my_final_dict[typ]={}
+
             print('for type ', typ)
             print('text_weight', my_dictr2[typ]["text_weight"])
             print('pos_weight', my_dictr2[typ]["pos_weight"])
@@ -106,11 +144,18 @@ for typ in my_dictr2:
             print('k', my_dictr2[typ]["k"])
             print('bump_weight', my_dictr2[typ]["bump_weight"])
             added.append(typ)
-            my_final_dict[typ]=my_dictr2[typ]
+            my_final_dict[typ]['POS']=my_dictr2[typ]["pos_weight"]
+            my_final_dict[typ]['ENT']=my_dictr2[typ]["ent_weight"]
+            my_final_dict[typ]['BUMP']=my_dictr2[typ]["bump_weight"]
+            my_final_dict[typ]['K']=my_dictr2[typ]["k"]
+            my_final_dict[typ]['TEXT']=my_dictr2[typ]["text_weight"]
+
 print('d')
 for typ in my_dictd:
     if my_dictd[typ]["text_weight"]!=0:
         if typ not in added:
+            my_final_dict[typ]={}
+
             print('for type ', typ)
             print('text_weight', my_dictd[typ]["text_weight"])
             print('pos_weight', my_dictd[typ]["pos_weight"])
@@ -118,7 +163,12 @@ for typ in my_dictd:
             print('k', my_dictd[typ]["k"])
             print('bump_weight', my_dictd[typ]["bump_weight"])
             added.append(typ)
-            my_final_dict[typ]=my_dictd[typ]
+            my_final_dict[typ]['POS']=my_dictd[typ]["pos_weight"]
+            my_final_dict[typ]['ENT']=my_dictd[typ]["ent_weight"]
+            my_final_dict[typ]['BUMP']=my_dictd[typ]["bump_weight"]
+            my_final_dict[typ]['K']=my_dictd[typ]["k"]
+            my_final_dict[typ]['TEXT']=my_dictd[typ]["text_weight"]
+
 # to_use_qtype=["where ADP","what ADJ", "how VERB","when PRON","how ADJ", "what VERB", "how AUX", "why VERB","what NUM", "when AUX", "who ADV"]
 
 
